b30abe16f5b475327c458d8f2e2ed2949ef9699e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-09-07 10:06:30 -0300

    fix: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 export function App() {
-  return <h1 style={{ fontFamily: 'Nunito, sans-serif' }} >Ignite Github Blog</h1>
+  return (
+    <h1 style={{ fontFamily: 'Nunito, sans-serif' }}>Ignite Github Blog</h1>
+  )
 }
