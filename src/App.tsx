a07fba3e726d32486b74a29a0f7b1595b8f89211
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-09-09 20:49:21 -0300

    feat: add app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 
-import { Home } from './pages/Home'
+import { AppRoutes } from './Routes'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 
@@ -8,7 +8,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <Home />
+      <AppRoutes />
     </ThemeProvider>
   )
 }
