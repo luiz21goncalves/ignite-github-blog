fd7a2623e31634a3f67ae56725a82321956db237
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-09-11 10:20:37 -0300

    chore: add react-query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ThemeProvider } from 'styled-components'
 
 import { AppRoutes } from './Routes'
+import { queryClient } from './services/queryClient'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 
@@ -8,7 +11,10 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
