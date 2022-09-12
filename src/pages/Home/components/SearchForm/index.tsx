df87a960947a778c78ff1048ddb1c03ad8ffcb77
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-09-12 17:17:17 -0300

    chore: register search input

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,17 +1,38 @@
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
 import { useIssues } from '../../../../contexts/IsseusContexts'
 import * as S from './styles'
 
+const SearchFormSchema = z.object({
+  query: z.string().min(1),
+})
+
+type SearchFormInputs = z.infer<typeof SearchFormSchema>
+
 export function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchFormInputs>()
+
   const { issues } = useIssues()
 
+  async function handleSearchIssue(data: SearchFormInputs) {
+    const { query } = data
+
+    console.log({ query })
+  }
+
   return (
-    <S.SearchFormContainer>
+    <S.SearchFormContainer onSubmit={handleSubmit(handleSearchIssue)}>
       <S.SearchFormHeader>
         <p>Publicações</p>
         <span>{issues.length} publicações</span>
       </S.SearchFormHeader>
 
-      <S.SearchFormInput type="text" placeholder="Buscar conteúdo" />
+      <S.SearchFormInput
+        type="text"
+        placeholder="Buscar conteúdo"
+        {...register('query')}
+      />
     </S.SearchFormContainer>
   )
 }
